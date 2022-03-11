5cbd52f0499b4c6ee58c2e20fb0b251eb8d40c89
yonispeed <yonaschali20@gmail.com>
2022-03-11 03:27:26 +0300

    hh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,20 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
 
 /**
- * main - Prints quote to the standard error
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: zero on success
- *
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
-}
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+} *
+
+
