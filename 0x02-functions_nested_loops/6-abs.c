01e44a210e1ed8fd3e2b43ad58ad8a5744a6db5b
yonispeed <yonaschali20@gmail.com>
2022-03-16 22:47:26 +0300

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Checks if a character is lowercase.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else
+return (-n);
+}
