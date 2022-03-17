296b40b3cd7ab1d6c50eb527ed868bf1fc241d87
yonispeed <yonaschali20@gmail.com>
2022-03-18 00:33:11 +0300

    ll

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+[200~#include "main.h"
+
+/**
+ *  * main - Prints _putchar with _putchar
+ *   * Description: Prints _putchar with _putchar
+ *    * Return: Always 0 (Success)
+ *     */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
