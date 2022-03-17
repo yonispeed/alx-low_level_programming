c70d7cbc73c58dd5d0428c51b224876d3eaa9aa1
yonispeed <yonaschali20@gmail.com>
2022-03-18 00:11:39 +0300

    l

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
