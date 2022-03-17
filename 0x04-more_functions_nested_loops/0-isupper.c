5455c0f695dd23c17d4006cc35ae48b63b7078a8
yonispeed <yonaschali20@gmail.com>
2022-03-18 00:30:42 +0300

    l

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
+ * main - Prints _putchar with _putchar
+ * Description: Prints _putchar with _putchar
  * Return: Always 0 (Success)
  */
 int _isupper(int c)
 {
 return (c >= 'A' && c <= 'Z');
 }
-
