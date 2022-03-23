b2229c32dfb7b225800439c0dc2a5397e9155312
yonispeed <yonaschali20@gmail.com>
2022-03-23 23:05:20 +0300

    n

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 01234567891011121314\n x10
+ *
+ * Return: void
+ */
+
+
+void more_numbers(void)
+{
+	int j;
+	int n;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		for (n = 0 ; n < 15 ; n++)
+		{
+			if (n > 9)
+				_putchar ((n / 10) + '0');
+			_putchar (n % 10 + '0');
+		}
+		_putchar ('\n');
+	}
+}
