ceada8edcca48ea37dc26cd5ba03583da6b8880a
yonispeed <yonaschali20@gmail.com>
2022-03-23 23:26:33 +0300

    m

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change the value of passed argument to
+ *				98 by derefrence it.
+ * @n: accepts address of the passed argument its a pointer.
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
