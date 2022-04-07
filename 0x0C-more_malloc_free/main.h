bf4df1cfa1cb8c0b054ef227e70b27aab84ebcfd
yonispeed <yonaschali20@gmail.com>
2022-04-08 00:54:40 +0300

    mlc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,18 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include "main.h"
+#include <stdlib.h>
 
-int _putchar(char);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
 
-#endif /* _MAIN_H_ */
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
